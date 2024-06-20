e4de0853f74ad393a0a4168012c5e97f5a1caed1
Xavier <kevinjkevps4@gmail.com>
2024-06-19 20:13:25 -0500

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "Programacion Distribuida"
+include("ejemplo01_cdi")
+include("ejemplo02_jpa")
+include("ejemplo03_rest")
+include("ejemplo03_spring")
+include("ejemplo05_quarkus")
+include("ejemplo05_autores")
